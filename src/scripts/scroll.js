698d6244b53fff29002a64bb6f0dde4e7e6950c8
MikeBloomfield <stashuseinov@gmail.com>
2021-12-17 22:01:32 +0200

    done

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,20 +1,20 @@
-(function() {
+(function () {
 
-    const smoothScroll = function(targetEl, duration) {
+    const smoothScroll = function (targetEl, duration) {
         const headerElHeight = document.querySelector('.header').clientHeight;
         let target = document.querySelector(targetEl);
         let targetPosition = target.getBoundingClientRect().top - headerElHeight;
         let startPosition = window.pageYOffset;
         let startTime = null;
 
-        const ease = function(t, b, c, d) {
+        const ease = function (t, b, c, d) {
             t /= d / 2;
             if (t < 1) return c / 2 * t * t + b;
             t--;
             return -c / 2 * (t * (t - 2) - 1) + b;
         };
 
-        const animation = function(currentTime) {
+        const animation = function (currentTime) {
             if (startTime === null) startTime = currentTime;
             const timeElapsed = currentTime - startTime;
             const run = ease(timeElapsed, startPosition, targetPosition, duration);
@@ -25,10 +25,10 @@
 
     };
 
-    const scrollTo = function() {
+    const scrollTo = function () {
         const links = document.querySelectorAll('.js-scroll');
         links.forEach(each => {
-            each.addEventListener('click', function() {
+            each.addEventListener('click', function () {
                 const currentTarget = this.getAttribute('href');
                 smoothScroll(currentTarget, 1000);
             });
@@ -36,3 +36,14 @@
     };
     scrollTo();
 }());
+
+
+(function () {
+    const header = document.querySelector('.header-inner');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add("header-inner-active")
+        } else { header.classList.remove("header-inner-active") }
+    };
+}());
+
