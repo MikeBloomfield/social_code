c624a29696bcaa7029295c00ae2ccde9ba9f1fa8
N-Team <n-team@MacBook-Pro-N-Team.local>
2022-01-05 17:04:54 +0200

    add animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,9 @@ import '../styles/styles.css'
 import json from '../assets/json'
 import './scroll.js'
 import 'animate.css'
-import wow from 'wowjs';
+import WOW from 'wowjs'
 
 
 let one = 1;
 console.log(one);
-
 console.log(json);
