198df4df9e1394a78900d4b40940bbf755ec445b
N-Team <n-team@MacBook-Pro-N-Team.local>
2022-01-06 16:43:17 +0200

    animation is done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,6 @@
+
 import '../styles/styles.css'
-import json from '../assets/json'
 import './scroll.js'
 import 'animate.css'
-import WOW from 'wowjs'
-
-
-let one = 1;
-console.log(one);
-console.log(json);
+import WOW from 'wowjs';
+new WOW.WOW().init();
