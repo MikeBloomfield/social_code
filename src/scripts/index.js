5319a045194e44b8b626982d4ae85a7bd1da3565
N-Team <n-team@MacBook-Pro-N-Team.local>
2022-01-11 17:47:03 +0200

    done accordion

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,15 @@
 import 'animate.css';
 import '../styles/styles.css'
+import './accordion.js'
 import './scroll.js'
 import WOW from 'wowjs';
+
+
 new WOW.WOW().init();
+
+$(document).ready(function() {
+    $('.accordion__item-title').click(function(event) {
+        $('.accordion__item').toggleClass('accordion__item--active');
+        $('.accordion__item-content').toggleClass('accordion__item-content--show');
+    });
+});
