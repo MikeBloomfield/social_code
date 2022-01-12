0e7f9c41ff74dbc39fa6611391a7a12e7d42ca2a
N-Team <n-team@MacBook-Pro-N-Team.local>
2022-01-12 16:08:09 +0200

    add sections and animation

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -4,15 +4,28 @@ $(document).ready(function() {
     $('.accordion__item-title').click(function(event) {
         const parent = $(this).parent();
         const sib = $(this).siblings();
-        if (parent.hasClass('accordion__item--active') ||
-        sib.hasClass('accordion__item--active') ) {
-            parent.removeClass('accordion__item--active')
-            sib.removeClass('accordion__item-content--show')
+        const plus = $(this).children('.accordion__item-title--plus');
+        const minus = $(this).children('.accordion__item-title--minus');
+
+
+
+        if (parent.hasClass('accordion__item--active')) { 
+
+            parent.removeClass('accordion__item--active');
+            sib.removeClass('accordion__item-content--show');
+            plus.removeClass('collapse');
+            minus.removeClass('show');
+
         } else {
+
             $('.accordion__item').removeClass('accordion__item--active');
-            parent.addClass('accordion__item--active');
+            parent.addClass('accordion__item--active').slideDown(200);
             $('.accordion__item-content').removeClass('accordion__item-content--show');
-            sib.addClass('accordion__item-content--show');
+            sib.addClass('accordion__item-content--show').slideDown(200);
+            $('.accordion__item-title--plus').removeClass('collapse');
+            plus.addClass('collapse');
+            $('.accordion__item-title--minus').removeClass('show');
+            minus.addClass('show');
         }
     });
 });
