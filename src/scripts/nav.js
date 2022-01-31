d4c4d25b342b574a2e191574abd20d28f1b5c0eb
N-Team <n-team@MacBook-Pro-N-Team.local>
2022-01-31 12:35:08 +0200

    already done

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -2,16 +2,12 @@
     const burgerItem = document.querySelector('.burger-menu');
     const menu = document.querySelector('.nav');
     const menuCloseItem = document.querySelector('.nav__close');
-    const link = document.querySelectorAll('.nav__list-item');
     
     burgerItem.addEventListener('click',() => {
         menu.classList.add('nav--active');
     });
     menuCloseItem.addEventListener('click',() => {
         menu.classList.remove('nav--active');
-    });
-    link.addEventListener('click',() => {
-        menu.classList.toggle('nav--active');
     });
     
 }());
